dfb4c0989aae5cc57b7bd90be84bf3432bab7195
Rasyiqah Amalina <rsssyqh@gmail.com>
2025-06-12 12:44:39 +0700

    simpan perubahan

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Testimoni;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -14,6 +15,12 @@ class PageController extends Controller
     public function booking()
     {
         return view('pages.book-a-table');
+    }
+
+    public function home()
+    {
+        $testimoni = Testimoni::where('status', 1)-> get();
+        return view('pages.home', compact('testimoni'));
     }
 
     public function testimoni()
