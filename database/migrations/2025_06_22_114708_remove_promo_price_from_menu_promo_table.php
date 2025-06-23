4c636804cea62108d53ea950ba0eee218c219080
Nazwa Sahara <nazwasahraa15@gmail.com>
2025-06-23 12:53:04 +0700

    fix merge conflict

--- database/migrations/2025_06_22_114708_remove_promo_price_from_menu_promo_table.php
+++ database/migrations/2025_06_22_114708_remove_promo_price_from_menu_promo_table.php
@@ -6,34 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
-<<<<<<<< HEAD:database/migrations/2025_06_22_113315_add_photo_to_users_table.php
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('photo')->nullable()->after('no_hp');
+        Schema::table('menu_promo', function (Blueprint $table) {
+            $table->dropColumn('promo_price');
         });
-========
-       Schema::table('menu_promo', function (Blueprint $table) {
-        $table->dropColumn('promo_price');
-    });
->>>>>>>> main:database/migrations/2025_06_22_114708_remove_promo_price_from_menu_promo_table.php
     }
 
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-<<<<<<<< HEAD:database/migrations/2025_06_22_113315_add_photo_to_users_table.php
-        Schema::table('users', function (Blueprint $table) {
-========
         Schema::table('menu_promo', function (Blueprint $table) {
->>>>>>>> main:database/migrations/2025_06_22_114708_remove_promo_price_from_menu_promo_table.php
-            //
+            $table->integer('promo_price')->nullable(); // sesuaikan tipe datanya
         });
     }
 };
